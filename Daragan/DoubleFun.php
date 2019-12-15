e4e695117375b0eab4fd8836968506f12d5e3e89
Tumbochka111 <Tumbochka111@users.noreply.github.com>
2019-12-15 21:40:26 +0300

    2

---
+++ Daragan/DoubleFun.php
@@ -0,0 +1,30 @@
+<?php namespace  Daragan;
+use core\EquationInterface;
+class DoubleFun extends LineFun implements EquationInterface{
+	protected $result = array();
+	protected function dis($a, $b, $c)
+	{
+		return ($b*$b)-4*$a*$c;
+	}
+	public function doubl($a, $b, $c)
+	{
+		if ($a==0){
+			return $this->line($b, $c);
+		}
+		DaraganLog::log("It is quadratic equation");
+		
+		$d=$this->dis($a, $b, $c);
+		
+		if($d < 0) {
+			throw new DaraganException("It is determined that such an equation does not exist. \n");
+		}
+		if($d==0){
+			$this->result[]=-$b/(2*$a);
+		}elseif($d>0){
+			$this->result=array((-$b+sqrt($d))/(2*$a), (-$b-sqrt($d))/(2*$a));
+		}
+		return $this->result;
+	}
+}
+
+?>
---
+++ index2.php
@@ -0,0 +1,100 @@
+<?php
+/*Interface classAInterface{
+	
+}*/
+//ax+b=0  count b=-1
+       class lineFun{
+	function __construct($result=0)
+	{
+		$this->result=$result;
+	}
+	protected $result;
+	function line($a, $b)
+	{if($a!=null){
+		$this->result=-$b/$a;}else echo 'Ты лох, уравнение не решаемо';
+	}
+	function getResult()
+	{
+	 
+		return $this->result;
+	}
+	function setResult($n1)
+	{
+		$this->result=$n1;
+	}
+}
+$a4= new lineFun();
+$a4->line(4, -6);
+echo($a4->getResult());
+
+
+
+
+class doubleFun extends lineFun {
+	protected $kor;
+	protected $D;
+	protected $x;
+	function discrim($a, $b, $c)
+	{
+		$this->D=($b*$b)-4*$a*$c;
+		if($this->D<0){$this->kor=0;}elseif($this->D=0){$this->kor=1;}else{$this->kor=2;}
+	}
+	function faindKor($a, $b, $c)
+	{
+		discrim($a, $b, $c);
+		if($this->kor>0){
+			$x1=(-$b+($this->D/$this->D))/(2*$a);
+			$this->x=array( $x1);
+			if($this->kor>1){
+				$x2=(-$b-($this->D/$this->D))/(2*$a);
+				$this->x[] =$x2;
+			}
+		}
+		else echo 'This is not it possible';
+	}
+	function doubl($a, $b, $c){
+		if ($a==0) {
+			echo "Это не квадратное уравнение, а линейное";
+			line($b, $c);
+		}
+		else {
+			faindKor($a, $b, $c);
+			$this->result="x1= implode(' x2= ', $this->x)";
+		}
+	}	
+}
+
+$a5= new doubleFun();
+$a5->doubl(1, 4, -6);
+echo($a5->getResult());
+
+
+
+/*class A extends B implements classAInterface{
+	protected $n1;
+	protected $n2;
+	function __construct($n1, $n2){
+		$this->n1=$n1;
+		$this->n2=$n2;
+	}
+	function getn1(){
+		return this->n1
+	}
+	
+	function setn2($n2){
+		this->n2=$n2}
+}
+class C extends A{
+	protected $n3;
+	function __construct($n1, $n2, $n3){
+		$this->n3=$n3;
+		parent::__construct($n1, $n2);
+	}
+
+
+$a4= new B(3);
+$a5= new B(6);//ax^2+cx=0
+$a2= new A($a4, $a5);
+$a3= new B(9);
+$a1= new A($a2, $a3);}*/
+?>
